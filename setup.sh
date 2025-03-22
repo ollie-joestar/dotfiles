ba7ed2cce7e4fa97430ea833ecd3e563f6e61072
Ollie Joestar <bigzzzid@gmail.com>
2025-03-22 17:05:21 +0100

    Another test, almost good

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Set up XDG_CONFIG_HOME
 
 export XDG_CONFIG_HOME="$HOME"/.config
-mkdir -p "XDG_CONFIG_HOME"
-mkdir -p "XDG_CONFIG_HOME"/nixpkgs
+mkdir -p "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
 # Create symlinks for existing configuration files
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
@@ -16,6 +16,8 @@ ln -sf "$PWD/.tmux" "$HOME"/.tmux
 ln -sf "$PWD/.zshrc" "$HOME"/.zshrc
 ln -sf "$PWD/.gitconfig" "$HOME"/.gitconfig
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
+ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
+ln -sf "$PWD/.zshenv" "$HOME"/.zshenv
 
 # Install Nix Packages from config.nix
 # nix-env -iA nixpkgs.myPackages

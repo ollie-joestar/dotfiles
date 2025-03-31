74d6960014d12dc09ae0517ce4533f97c841de31
Ollie Joestar <bigzzzid@gmail.com>
2025-03-31 12:06:22 +0200

    kanagawa kitty fix

--- setup.sh
+++ setup.sh
@@ -13,18 +13,18 @@ mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/.vimrc" "$HOME"/.vimrc
 ln -sf "$PWD/.config/starship.toml" "$XDG_CONFIG_HOME"/starship.toml
-ln -sf "$PWD/.config/kitty" "$XDG_CONFIG_HOME"/kitty
+# ln -sf "$PWD/.config/kitty" "$XDG_CONFIG_HOME"/kitty
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/.tmux" "$HOME"/.tmux
-ln -sf "$PWD/.zshrc" "$HOME"/.zshrc
-ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
-ln -sf "$PWD/.zshenv" "$HOME"/.zshenv
-ln -sf "$PWD/.gitconfig" "$HOME"/.gitconfig
-ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
-tic -x "$PWD/xterm-kitty.terminfo"
+# ln -sf "$PWD/.zshrc" "$HOME"/.zshrc
+# ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
+# ln -sf "$PWD/.zshenv" "$HOME"/.zshenv
+# ln -sf "$PWD/.gitconfig" "$HOME"/.gitconfig
+# ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
+# tic -x "$PWD/xterm-kitty.terminfo"
 
 # Install Nix Packages from config.nix
-nix-env -iA nixpkgs.myPackages
+# nix-env -iA nixpkgs.myPackages
 
 # packages=(
 # 	tmux
